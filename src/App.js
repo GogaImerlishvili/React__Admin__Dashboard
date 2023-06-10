0da1559d9339b0390c6a1d449cedad25f7a54ed5
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2023-06-10 17:35:20 +0400

    added team page with path

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Routes,Route} from "react-router-dom"
 import Topbar from "./scenes/global/Topbar"
 import SideBar from "./scenes/global/SideBar";
 import Dashboard from "./scenes/dashboard";
-// import Team from "./scenes/team";
+import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
@@ -34,7 +34,7 @@ function App() {
     <Routes>
     
       <Route path="/" element={<Dashboard />} />
-      {/* <Route path="/team" element={<Team />} /> */}
+      <Route path="/team" element={<Team />} />
       {/* <Route path="/contacts" element={<Contacts />} /> */}
       {/* <Route path="/invoices" element={<Invoices />} /> */}
       {/* <Route path="/form" element={<Form />} /> */}
