9e3311436efdf93b8e78a22e5a1c45c69e765778
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2023-06-14 12:47:41 +0400

    import faq page with routes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import CalendarDate from "./scenes/calendar";
 
@@ -41,7 +41,7 @@ function App() {
       {/* <Route path="/bar" element={<Bar />} /> */}
       {/* <Route path="/pie" element={<Pie/>} /> */}
       {/* <Route path="/line" element={<Line />} /> */}
-      {/* <Route path="/faq" element={<FAQ />} />   */}
+      <Route path="/faq" element={<FAQ />} />  
       {/* <Route path="/geography" element={<Geography/>} /> */}
       <Route path="/calendar" element={<CalendarDate/>} /> 
     </Routes>
